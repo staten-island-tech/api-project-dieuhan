f06aab3019651ba631a1c6f63d46d54732408d7d
dieuhan <dieuhan.mut@gmail.com>
2023-12-08 13:09:55 -0500

    r245t4t4

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,14 +3,15 @@ import '../css/style.css'
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
 
 async function getData(URL){
-  try{
+  //try{
     const response = await fetch(URL);
     const cards = await response.json();
-    console.log(cards);
-    document.getElementById("api-response").textContent = cards.cards;
+    cards.results.forEach((cards)=> console.log(cards));
+  /*   document.getElementById("api-response").textContent = cards.cards;
     console.log(cards.cards); 
   } catch (error){
     console.log(error);
-  }
+  } */
 }
+
 getData(URL);
