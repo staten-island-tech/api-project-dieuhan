85603a0a4316d00fb66bd37a767eb0e4b4652af3
dieuhan <dieuhan.mut@gmail.com>
2023-12-10 15:09:59 -0500

    blpo

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,4 +1,7 @@
-const DOMSelectors = {
+export const DOMSelectors = {
     gallery: document.querySelector(".gallery"),
-    card: document.querySelector(".card")
+    card: document.querySelector(".card"),
+    h1: document.querySelector(".h1"),
+    h2: document.querySelector(".h2"),
+    h3: document.querySelector(".h3")
 };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,30 +1,36 @@
 import '../css/style.css'
+import { DOMSelectors } from './dom';
 
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
-
 async function getData(URL){
-  //try{
+  try{
     const response = await fetch(URL);
+    if (response.status != 200){
+      throw new Error(response.statusText)
+    }
     const cards = await response.json();
-    cards.results.forEach((cards)=> console.log(cards));
-  /*   document.getElementById("api-response").textContent = cards.cards;
-    console.log(cards.cards); 
+    insert(cards);
+/*     cards.results.forEach((cards)=> console.log(cards));
+    document.getElementById("api-response").textContent = cards.cards;
+    console.log(cards.cards);  */
   } catch (error){
-    console.log(error);
-  } */
+    console.log(error, "hell nah");
+    document.querySelector(".h1").textContent = "hell nah";
+  } 
 }
+
+getData(URL); 
 
 /*  Array.from.sync(getData).then((array)=> console.log(array)) */
 
-getData(URL); 
+
 
 
 function insert(array){
   array.forEach((object)=> {
     DOMSelectors.gallery.insertAdjacentHTML(
-      "afterbegin"
-      `<div class="card"><h2>${object.name}</h2>
+      `afterbegin`, `<div class="gallery"> 
+      <div class="card"><h2>${object.name}</h2>
+      </div>
       </div>`
   )})};
-
-  insert(pokemon)
