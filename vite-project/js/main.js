dda3fd37bcb70166d7a24dc6dd6f432b9a2ceff8
dieuhan <dieuhan.mut@gmail.com>
2023-12-18 13:13:09 -0500

    fwipghwo

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -15,33 +15,33 @@ function insert(arr){
 
 
 const URL = "https://data.cityofnewyork.us/resource/uq7m-95z8.json"
- async function getData(URL){
+/*  async function getData(URL){
   try{
     const response = await fetch(URL);
     if (response.status != 200) {
       throw new Error(response.statusText);
     }
     const data = await response.json();
-    insert(data.data)
+    insert(data)
     cards.forEach((cards)=> console.log(cards.school_name));
   } catch(error){
     console.log(error, "pls try again");
     document.querySelector("h1").textContent = "pls try again";
   }
-};
+}; */
  
-/* 
+
  async function getData(URL){
   try{
     const response = await fetch(URL);
     const cards = await response.json();
-    insert(cards.cards)
-    //cards.forEach((cards)=> console.log(cards.school_name, cards.borough));
+   insert(cards)
+    cards.forEach((cards)=> (cards.school_name, cards.borough));
   } catch(error){
     console.log(error);
   }
 };
- */
+
 getData(URL);
 
  
